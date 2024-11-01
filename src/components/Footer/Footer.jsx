53aaf632685a544cf16bac172adf9f1f72c0a1e9
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-11-01 19:46:58 +0600

    Add Footer component in Home component for footer design

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h1 className="footer_title">&copy; 2024 All rights reserved.</h1>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import './Home.css'
+import Footer from '../Footer/Footer';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home'>
             <Header></Header>
-            {/* Render the children components from the router or used in parent route elements to render their child route elements */}
-            <Outlet></Outlet>
+            <div className="home_child">
+                {/* Render the children components from the router or used in parent route elements to render their child route elements */}
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
