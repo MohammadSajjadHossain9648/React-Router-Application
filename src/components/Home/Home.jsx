a3168a9b8e8b274fda008f80722d274e2170854e
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-11-01 10:05:05 +0600

    Add Home component and set up routing in main.jsx

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home page</h1>
+        </div>
+    );
+};
+
+Home.propTypes = {
+
+};
+
+export default Home;
