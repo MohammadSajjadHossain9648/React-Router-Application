6476fa9154194517b57cdde2ede81cd6e04a0c94
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-11-01 11:11:18 +0600

    Add Outlet in Home component to render their child route elements

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
             <h1>Home page</h1>
+            {/* Render the children components from the router or used in parent route elements to render their child route elements */}
+            <Outlet></Outlet>
         </div>
     );
 };
